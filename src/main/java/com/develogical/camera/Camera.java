1f30b693d50fdd261d92fd412c3223ec44aa7a09
Byron <byron@bytemech.com>
2021-03-17 09:14:23 +0000

    Done

--- src/main/java/com/develogical/camera/Camera.java
+++ src/main/java/com/develogical/camera/Camera.java
@@ -1,17 +1,44 @@
 package com.develogical.camera;
 
+import sun.applet.resources.MsgAppletViewer_es;
+
 public class Camera {
+    private Sensor sensor;
+    private MemoryCard memoryCard;
+    private WriteCompleteListener writeCompleteListener;
+    private boolean powerState = false;
+    private boolean safeToPowerOff = false;
+
+    public Camera (Sensor sensor, MemoryCard memoryCard, WriteCompleteListener writeCompleteListener) {
+        this.sensor = sensor;
+        this.memoryCard = memoryCard;
+        this.writeCompleteListener = writeCompleteListener;
+    }
 
     public void pressShutter() {
-        // not implemented
+        if (powerState != false) {
+            byte[] data = this.sensor.readData();
+            this.memoryCard.write(data, this.writeCompleteListener);
+        } else {
+            return;
+        }
+    }
+
+    public void dataCopyCompleted() {
+        this.safeToPowerOff = true;
     }
 
     public void powerOn() {
-        // not implemented
+        this.powerState = true;
+        this.sensor.powerUp();
     }
 
     public void powerOff() {
-       // not implemented
+        writeCompleteListener.writeComplete();
+        if (safeToPowerOff) {
+            this.sensor.powerDown();
+            this.powerState = false;
+        }
     }
 }
 
--- src/test/java/com/develogical/camera/CameraTest.java
+++ src/test/java/com/develogical/camera/CameraTest.java
@@ -1,13 +1,78 @@
 package com.develogical.camera;
 
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-
 import static org.mockito.Mockito.*;
 
 public class CameraTest {
+
     @Test
+    //Switching the camera on powers up the sensor
     public void switchingTheCameraOnPowersUpTheSensor() {
-        // write your test here
+        Sensor sensor = mock(Sensor.class);
+        MemoryCard memorycard = mock(MemoryCard.class);
+        WriteCompleteListener writeCompleteListener = mock(WriteCompleteListener.class);
+        Camera underTest = new Camera(sensor,memorycard,writeCompleteListener);
+        underTest.powerOn();
+        verify(sensor).powerUp();
+    }
+
+    @Test
+    //Switching the camera off powers down the sensor
+    public void switchingTheCameraOffPowersDownTheSensor() {
+        Sensor sensor = mock(Sensor.class);
+        MemoryCard memorycard = mock(MemoryCard.class);
+        WriteCompleteListener writeCompleteListener = mock(WriteCompleteListener.class);
+        Camera underTest = new Camera(sensor,memorycard,writeCompleteListener);
+        underTest.dataCopyCompleted();
+        underTest.powerOff();
+        verify(sensor).powerDown();
+    }
+
+    @Test
+    //Pressing the shutter with the power on copies data from the sensor to the memory card
+    public void pressShutterandDataCopiestoMemoryCard() {
+        Sensor sensor = mock(Sensor.class);
+        MemoryCard memorycard = mock(MemoryCard.class);
+        WriteCompleteListener writeCompleteListener = mock(WriteCompleteListener.class);
+        Camera underTest = new Camera(sensor,memorycard,writeCompleteListener);
+        underTest.powerOn();
+        underTest.pressShutter();
+        verify(sensor).readData();
+        verify(memorycard).write(sensor.readData(), writeCompleteListener);
+    }
+
+    @Test
+    //Pressing the shutter when the power is off does nothing
+    public void pressShutterWhenPowerOffDoesNothing() {
+        Sensor sensor = mock(Sensor.class);
+        MemoryCard memorycard = mock(MemoryCard.class);
+        WriteCompleteListener writeCompleteListener = mock(WriteCompleteListener.class);
+        Camera underTest = new Camera(sensor,memorycard,writeCompleteListener);
+
+        underTest.pressShutter();
+        verifyNoMoreInteractions(sensor);
+    }
+
+    //If data is currently being written, switching the camera off does not power down the sensor
+    @Test
+    public void dataCurrentlyBeingWrittenDontPowerDownSensor() {
+        Sensor sensor = mock(Sensor.class);
+        MemoryCard memorycard = mock(MemoryCard.class);
+        WriteCompleteListener writeCompleteListener = mock(WriteCompleteListener.class);
+        Camera underTest = new Camera(sensor, memorycard, writeCompleteListener);
+        underTest.powerOff();
+        verifyNoMoreInteractions(sensor);
+    }
+
+    //Once writing the data has completed, then the camera powers down the sensor
+    @Test
+    public void dataFinishedWritingDoPowerDownSensor() {
+        Sensor sensor = mock(Sensor.class);
+        MemoryCard memorycard = mock(MemoryCard.class);
+        WriteCompleteListener writeCompleteListener = mock(WriteCompleteListener.class);
+        Camera underTest = new Camera(sensor, memorycard, writeCompleteListener);
+        underTest.dataCopyCompleted();
+        underTest.powerOff();
+        verify(sensor).powerDown();
     }
 }
